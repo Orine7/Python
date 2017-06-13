b6976aca0ff3cd583b10d8630f0efeab525ff4dc
Eduardo Sarmento Tenorio Filho <eduardofilho_08@hotmail.com>
2017-06-13 06:03:41 -0300

    Delete Desafio 6.py

--- Desafio 6.py
+++
@@ -1,4 +0,0 @@
-var = int(input('Digite um numero:'))
-print('O dobro desse numero é: {}'.format(var*2))
-print('O triplo desse numero é: {0}'.format(var*3))
-print('A raiz quadrada desse numero é: '.format(var**(1/2)))
