1c1755ab4d48a0b737a88ba844e9159019e2b8ce
Eduardo Sarmento Tenorio Filho <eduardofilho_08@hotmail.com>
2017-06-13 06:02:37 -0300

    Delete Desafio 11.py

--- Desafio 11.py
+++
@@ -1,3 +0,0 @@
-H = float(input("Qual a altura da parede?"))
-L = float(input("Qual a largura da parede?"))
-print('A quantidade de tinta necessaria para pintar a parede é:{} L de tinta'.format((H*L)/2))
